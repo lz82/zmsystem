e8d3cfa05fb3bc63f4ab7632cb7c1cc2d282f79c
lz82 <nickylau82@gmail.com>
2019-09-18 23:30:20 +0800

    :sparkles: init project

---
+++ js/redirect-legacy.a54611e2.js
@@ -0,0 +1,1 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["redirect"],{ef3c:function(e,r,t){"use strict";t.r(r);t("a481");var n,u,a={beforeCreate:function(){var e=this.$route,r=e.params,t=e.query,n=r.path;this.$router.replace({path:"/"+n,query:t})},render:function(e){return e()}},o=a,c=t("2877"),p=Object(c["a"])(o,n,u,!1,null,null,null);r["default"]=p.exports}}]);
